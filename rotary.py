66754c8ae618b2b7c4a5c9631de4ea513f13db87
Paul Cutler <paul@paulcutler.org>
2021-06-28 13:44:17 -0500

    Update rotary.py to test working with gpiozero module

--- rotary.py
+++ rotary.py
@@ -1,5 +1,12 @@
-import requests
-import denonavr
+
 from gpiozero import Button, RotaryEncoder
+from threading import Event
 
 
+mute_button = Button(13)
+
+while True:
+    if mute_button.is_pressed:
+        print("Button is pressed")
+    else:
+        print("Button is not pressed")
