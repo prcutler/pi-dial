3be0f2dd63e10aef0e496dd02d8e099a7510b230
Paul Cutler <paul@paulcutler.org>
2021-06-29 19:01:41 -0500

    Update rotary.py with v1 denonavr commands.  Needs to be tested

--- rotary.py
+++ rotary.py
@@ -8,21 +8,21 @@ mute_button = Button(13)
 # Set Denon AVR stats
 zones = {"Zone2": "Paul Office"}
 rec = denonavr.DenonAVR("192.168.1.119", name="Main Zone", add_zones=zones)
-rec.async_setup()
-rec.async_update()
+rec.update()
+rec.zones['Zone2'].update()
 
 # receiver = rec.zones['Zone2']
 print(rec.zones['Zone2'].mute, rec.zones['Zone2'].volume)
 #print(rec.zones)
 
-rec.zones['Zone2'].async_mute(False)
-rec.async_update()
+rec.zones['Zone2'].mute(False)
+rec.update()
 
 
 
 while True:
     if mute_button.is_pressed:
-        if rec.zones['Zone2'].mute is None:
+        if rec.zones['Zone2'].mute is True:
             rec.zones['Zone2'].mute(False)
             print("Turning off mute")
             rec.async_update()
