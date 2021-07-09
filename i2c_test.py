2c2dbcbcc3411075398417d7e1dfa424dfd80be2
Paul Cutler <paul@paulcutler.org>
2021-07-09 11:05:48 -0500

    Add Denon commands to display on LCD

--- i2c_test.py
+++ i2c_test.py
@@ -1,5 +1,6 @@
 from RPi_GPIO_i2c_LCD import lcd
 from time import sleep
+import denonavr
 
 ## Address of backpack
 i2c_address = 0x27
@@ -7,8 +8,17 @@ i2c_address = 0x27
 ## Initalize display
 lcdDisplay = lcd.HD44780(i2c_address)
 
+# Set Denon AVR stats
+zones = {"Zone2": "Paul Office"}
+rec = denonavr.DenonAVR("192.168.1.119", name="Main Zone", add_zones=zones)
+rec.update()
+rec.zones["Zone2"].update()
+
+zone2_volume = rec.zones["Zone2"].volume
+zone2_input = rec.zones["Zone2"].input
+
 ## Set string value to buffer
-lcdDisplay.set("Hello", 1)
-lcdDisplay.set("World", 2)
+lcdDisplay.set(zone2_volume, 1)
+lcdDisplay.set(zone2_input, 2)
 
 sleep(1)
