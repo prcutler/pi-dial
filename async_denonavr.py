45887856502e9a1d28171cea63622481280e3270
Paul Cutler <paul@paulcutler.org>
2021-07-13 15:00:56 -0500

    Start to refactor the async way.  Long way to go

--- async_denonavr.py
+++ async_denonavr.py
@@ -1,7 +1,24 @@
 import denonavr
+import asyncio
+
 
 d = denonavr.DenonAVR("192.168.1.119")
-d.async_setup()
-d.async_update()
+# d.async_setup()
+# d.async_update()
 
-print(d.volume)
+print("the volume is: ", d.volume)
+
+
+async def main():
+    async def setup_avr():
+
+        await d.async_setup()
+        await d.async_update()
+
+        print("Async method volume is: ", d.volume)
+
+
+# asyncio.run(setup_avr())
+
+if __name__ == "__main__":
+    asyncio.run(main)
