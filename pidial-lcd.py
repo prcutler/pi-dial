0ef761131d214dda1444bb448f9e7b017c7c5cb1
Paul Cutler <paul@paulcutler.org>
2021-10-23 16:07:24 -0500

    Refactor pidial-lcd switching to RPLCD library

--- pidial-lcd.py
+++ pidial-lcd.py
@@ -1,50 +1,43 @@
 from signal import pause
 from time import sleep
-
 import denonavr
-from RPi_GPIO_i2c_LCD import lcd
+from RPLCD.i2c import CharLCD
 
 ## Address of backpack
 i2c_address = 0x27
 
 ## Initalize display
-lcdDisplay = lcd.HD44780(i2c_address)
+lcd = CharLCD('PCF8574', 0x27)
 
-# Set Denon AVR stats
+# Connect to the Denon receiver
 zones = {"Zone2": "Paul Office"}
 rec = denonavr.DenonAVR("192.168.1.119", name="Main Zone", add_zones=zones)
 rec.update()
 rec.zones["Zone2"].update()
 
-# print(zone2_volume, zone2_input)
-
-
+# Method to display input and volume on LCD
 def lcd_query():
-    # Text to display on the LCD
+    # Poll the receiver
     rec.zones["Zone2"].update()
 
+    # Get the current volume and input
     zone2_volume = rec.zones["Zone2"].volume
     zone2_input = rec.zones["Zone2"].input_func
 
-    # Clear Display
-    lcdDisplay.clear()
-
     display_volume = "Volume: " + str(zone2_volume)
     display_input = "Input: " + str(zone2_input)
-    print("Zone2: volume ", zone2_volume, "Input: ", zone2_input)
 
-    ## Order text appears on the LCD:
-    lcdDisplay.set(display_volume, 1)
-    lcdDisplay.set(display_input, 2)
+    ## Write to the LCD (first clear it), write line one, line break, write line two
+    lcd.clear()
+    lcd.write_string(display_input)
+    lcd.crlf()
+    lcd.write_string(display_volume)
 
 
 def lcd_display():
 
     while True:
-
-        rec.zones["Zone2"].update()
-
-        sleep(5)
+        sleep(0.5)
         lcd_query()
 
 
