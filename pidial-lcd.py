88a3d68888ad70f9727fbeb727dbdeff39af2523
Paul Cutler <paul@paulcutler.org>
2021-11-25 16:22:18 -0600

    Fix while loop to detect state change correctly

--- pidial-lcd.py
+++ pidial-lcd.py
@@ -2,6 +2,7 @@ from time import sleep
 import time
 import denonavr
 from RPLCD.i2c import CharLCD
+from signal import pause
 
 ## Address of backpack
 i2c_address = 0x27
@@ -21,53 +22,52 @@ zone2_mute = rec.zones["Zone2"].muted
 
 # Mute Display
 framebuffer = [
-    "Test Line 1",
+    "",
     "",
 ]
 
-
-def write_muted(lcd, framebuffer, num_cols):
-    """Write the framebuffer out to the specified LCD."""
-    lcd.home()
-    for row in framebuffer:
-        lcd.write_string(row.ljust(num_cols)[:num_cols])
-        lcd.write_string("\r\n")
-
-
-write_muted(lcd, framebuffer, 16)
-
-mute_string = "Mute Engaged"
-
-
-def mute_loop(string, lcd, framebuffer, row, num_cols, delay=0.2):
-    padding = " " * num_cols
-    s = padding + string + padding
-    for i in range(len(s) - num_cols + 1):
-        framebuffer[row] = s[i : i + num_cols]
-        write_muted(lcd, framebuffer, num_cols)
-        time.sleep(delay)
-
-
 def lcd_display():
+    def write_muted(lcd, framebuffer, num_cols):
+        """Write the framebuffer out to the specified LCD."""
+        lcd.home()
+        for row in framebuffer:
+            lcd.write_string(row.ljust(num_cols)[:num_cols])
+            lcd.write_string("\r\n")
+
+    write_muted(lcd, framebuffer, 16)
+
+    mute_string = "Mute Engaged"
+
+    def mute_loop(string, lcd, framebuffer, row, num_cols, delay=0.2):
+        padding = " " * num_cols
+        s = padding + string + padding
+        for i in range(len(s) - num_cols + 1):
+            framebuffer[row] = s[i : i + num_cols]
+            write_muted(lcd, framebuffer, num_cols)
+            time.sleep(delay)
+
+#    mute_loop(mute_string, lcd, framebuffer, 0, 16)
 
     while True:
 
-        zone2_mute = rec.zones["Zone2"].muted
+        rec.zones["Zone2"].update()
 
-        if zone2_mute == True:
+        if rec.zones["Zone2"].muted is True:
             lcd.clear()
+            print("Muting")
             mute_loop(mute_string, lcd, framebuffer, 1, 16)
-
+            
         else:
             display_volume = "Volume: " + str(zone2_volume)
             display_input = "Input: " + str(zone2_input)
 
-        ## Write to the LCD (first clear it), write line one, line break, write line two
-        lcd.clear()
-        lcd.write_string(display_input)
-        lcd.crlf()
-        lcd.write_string(display_volume)
-        sleep(3)
+            ## Write to the LCD (first clear it), write line one, line break, write line two
+            lcd.clear()
+            lcd.write_string(display_input)
+            lcd.crlf()
+            lcd.write_string(display_volume)
+            sleep(3)
+
 
 
 if __name__ == "__main__":
