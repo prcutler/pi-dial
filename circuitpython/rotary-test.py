563fe2be64373b3643190bf969556f8e649b0476
Paul Cutler <paul@paulcutler.org>
2022-03-13 07:23:07 -0500

    Add rotary encoder test file

---
+++ circuitpython/rotary-test.py
@@ -0,0 +1,14 @@
+# SPDX-FileCopyrightText: 2018 Kattni Rembor for Adafruit Industries
+#
+# SPDX-License-Identifier: MIT
+
+import rotaryio
+import board
+
+encoder = rotaryio.IncrementalEncoder(board.D10, board.D9)
+last_position = None
+while True:
+    position = encoder.position
+    if last_position is None or position != last_position:
+        print(position)
+    last_position = position
