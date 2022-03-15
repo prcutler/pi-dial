259c4e357293a48ce20ccf4264aa837cc9aa8dcb
Paul Cutler <paul@paulcutler.org>
2022-03-14 20:38:25 -0500

    Continue to test asyncio methods - fix volume

--- async-test.py
+++ async-test.py
@@ -11,7 +11,7 @@ rec = denonavr.DenonAVR("192.168.1.119", name="Main Zone", add_zones=zones)
 rec.async_update()
 rec.zones["Zone2"].async_update()
 # rec_volume = float(rec.zones["Zone2"].volume)
-rec_volume = rec.zones["Zone2"].set_volume(40)
+rec_volume = rec.zones["Zone2"].set_volume(-40.0)
 
 # Zone 2 information
 zone2_volume = rec.zones["Zone2"].volume
